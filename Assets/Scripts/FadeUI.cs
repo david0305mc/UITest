804fdf4c6fc8b7117af2358dc81c8b954336b508
WCN김문창\WCN-P-00-00-088 <kmchman@naver.com>
2022-07-29 12:21:45 +0900

    Added FadeUI

---
+++ Assets/Scripts/FadeUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FadeUI : MonoBehaviour
+{
+
+    private bool isFaded = false;
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    public void Fader()
+    {
+        isFaded = !isFaded;
+
+        if (isFaded)
+        {
+            canvasGroup.DOFade(0, 2);
+        }
+        else
+        {
+            canvasGroup.DOFade(1, 2);
+        }
+    }
+
+}
