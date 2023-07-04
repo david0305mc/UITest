fce92604506736e152d2b4b662c3b0cfb533c356
david0305 <kmchman@naver.com>
2023-07-04 13:24:35 +0900

    Added CityBuilder

--- Assets/Scripts/GridPlaceObject/ObjFollowMouse.cs
+++ Assets/Scripts/GridPlaceObject/ObjFollowMouse.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using GridBuilding;
+
 public class ObjFollowMouse : MonoBehaviour
 {
     private PlaceObjectOnGrid placeObjectOnGrid;
--- Assets/Scripts/GridPlaceObject/PlaceObjectOnGrid.cs
+++ Assets/Scripts/GridPlaceObject/PlaceObjectOnGrid.cs
@@ -3,90 +3,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaceObjectOnGrid : MonoBehaviour
+namespace GridBuilding
 {
-    public Transform gridCellPrefab;
-    public Transform cube;
-    public Transform onMousePrefab;
-    private Node[,] nodes;
-    public Vector3 smoothMousePosition;
+    public class PlaceObjectOnGrid : MonoBehaviour
+    {
+        public Transform gridCellPrefab;
+        public Transform cube;
+        public Transform onMousePrefab;
+        private Node[,] nodes;
+        public Vector3 smoothMousePosition;
 
-    [SerializeField] private int height;
-    [SerializeField] private int width;
-    private Plane plane;
-    private Vector3 mousePosition;
+        [SerializeField] private int height;
+        [SerializeField] private int width;
+        private Plane plane;
+        private Vector3 mousePosition;
 
-    private void Start()
-    {
-        CreateGrid();
-        plane = new Plane(Vector3.up, transform.position);
-    }
-    private void Update()
-    {
-        GetMousePositionOnGrid();
-    }
-    private void GetMousePositionOnGrid()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (plane.Raycast(ray, out var enter))
+        private void Start()
         {
-            mousePosition = ray.GetPoint(enter);
-            smoothMousePosition = mousePosition;
-            mousePosition.y = 0;
-            mousePosition = Vector3Int.RoundToInt(mousePosition);
-
-            foreach (var node in nodes)
+            CreateGrid();
+            plane = new Plane(Vector3.up, transform.position);
+        }
+        private void Update()
+        {
+            GetMousePositionOnGrid();
+        }
+        private void GetMousePositionOnGrid()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out var enter))
             {
-                if (node.cellPosition == mousePosition && node.isPlaceable)
+                mousePosition = ray.GetPoint(enter);
+                smoothMousePosition = mousePosition;
+                mousePosition.y = 0;
+                mousePosition = Vector3Int.RoundToInt(mousePosition);
+
+                foreach (var node in nodes)
                 {
-                    if (Input.GetMouseButtonUp(0) && onMousePrefab != null)
+                    if (node.cellPosition == mousePosition && node.isPlaceable)
                     {
-                        node.isPlaceable = false;
-                        onMousePrefab.GetComponent<ObjFollowMouse>().isOnGrid = true;
-                        onMousePrefab.position = node.cellPosition + new Vector3(0, 0.5f, 0);
-                        onMousePrefab = null;
+                        if (Input.GetMouseButtonUp(0) && onMousePrefab != null)
+                        {
+                            node.isPlaceable = false;
+                            onMousePrefab.GetComponent<ObjFollowMouse>().isOnGrid = true;
+                            onMousePrefab.position = node.cellPosition + new Vector3(0, 0.5f, 0);
+                            onMousePrefab = null;
+                        }
                     }
+                }
+            }
+        }
+
+        public void OnMouseClickOnUI()
+        {
+            if (onMousePrefab == null)
+            {
+                onMousePrefab = Instantiate(cube, mousePosition, Quaternion.identity);
+            }
+        }
+
+        private void CreateGrid()
+        {
+            nodes = new Node[width, height];
+            var name = 0;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Vector3 worldPosition = new Vector3(i, 0, j);
+                    Transform obj = Instantiate(gridCellPrefab, worldPosition, Quaternion.identity);
+                    obj.name = "Cell" + name;
+                    nodes[i, j] = new Node(true, worldPosition, obj);
+                    name++;
                 }
             }
         }
     }
 
-    public void OnMouseClickOnUI()
+    public class Node
     {
-        if (onMousePrefab == null)
+        public bool isPlaceable;
+        public Vector3 cellPosition;
+        public Transform obj;
+
+        public Node(bool isPlaceable, Vector3 cellPosition, Transform obj)
         {
-            onMousePrefab = Instantiate(cube, mousePosition, Quaternion.identity);
+            this.isPlaceable = isPlaceable;
+            this.cellPosition = cellPosition;
+            this.obj = obj;
         }
     }
 
-    private void CreateGrid()
-    {
-        nodes = new Node[width, height];
-        var name = 0;
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                Vector3 worldPosition = new Vector3(i, 0, j);
-                Transform obj = Instantiate(gridCellPrefab, worldPosition, Quaternion.identity);
-                obj.name = "Cell" + name;
-                nodes[i, j] = new Node(true, worldPosition, obj);
-                name++;
-            }
-        }
-    }
-}
-
-public class Node
-{
-    public bool isPlaceable;
-    public Vector3 cellPosition;
-    public Transform obj;
-
-    public Node(bool isPlaceable, Vector3 cellPosition, Transform obj)
-    {
-        this.isPlaceable = isPlaceable;
-        this.cellPosition = cellPosition;
-        this.obj = obj;
-    }
 }
